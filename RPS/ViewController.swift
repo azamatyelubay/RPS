28371f94d5803625402047baff699419e3bc74c3
Yelubay Azamat <yelubayazamat@gmail.com>
2020-06-20 14:50:26 +0600

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Azamat Yelubay on 6/20/20.
+//  Copyright © 2020 Azamat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
