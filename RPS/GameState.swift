b0b1f59d06da56f4967a7d17fa9ad2249e1aa397
Yelubay Azamat <yelubayazamat@gmail.com>
2020-06-21 17:08:33 +0600

    Full project

---
+++ RPS/GameState.swift
@@ -0,0 +1,13 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by Azamat Yelubay on 6/20/20.
+//  Copyright © 2020 Azamat. All rights reserved.
+//
+
+import Foundation
+
+enum GameState {
+    case start , win , lose , draw 
+}
